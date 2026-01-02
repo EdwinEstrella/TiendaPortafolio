f1bcc1800f8962ef7f8a36f1e3473bf7973abcd3
EdwinEstrella <edwinestrella67@gmail.com>
2026-01-02 09:04:07 -0400

    feat: configure Vite preview server to allow specific hosts

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: ["webs-tienda-portafolio-5ectpg-2a9cd9-190-166-109-120.traefik.me"],
+  },
 })
